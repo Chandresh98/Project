9b40883ca932c9e54bebb12bd9e5fea85f684e7f
Chandresh98 <99880909+Chandresh98@users.noreply.github.com>
2022-04-06 02:00:28 +0530

    upload with correct code

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,7 +1,7 @@
 const urlModel = require("../models/url")
-const  { nanoid } = require('nanoid')
+const { nanoid } = require('nanoid')
 
-const isvalid = function(value) {
+const isvalid = function (value) {
     if (typeof value == undefined || typeof value == null) { return false }
     if (typeof value == 'string' && value.trim().length == 0) { return false }
     return true
@@ -13,32 +13,27 @@ const posturl = async function (req, res) {
 
 
     try {
-        if(Object.keys(req.body).length==0){
-            return res.status(400).send({Status:false , msg:"Provide input"})}
-        
-            let longUrl = req.body.url
-        if (! isvalid(longUrl) ) {
+        if (Object.keys(req.body).length == 0) {
+            return res.status(400).send({ Status: false, msg: "Provide input" })
+        }
+
+        let longUrl = req.body.longUrl
+        if (!isvalid(longUrl)) {
             return res.status(400).send({ Status: false, ERROR: "Please provide a url field and enter url" })
         }
-        if(! re.test(longUrl)){return res.status(400).send({Status:false , msg:"Please provide a valid url"})}
+        if (!re.test(longUrl)) { return res.status(400).send({ Status: false, msg: "Please provide a valid url" }) }
 
-        let dupliUrl = await urlModel.findOne({longUrl:longUrl})
-        if(dupliUrl){return res.status(200).send({Status:true , msg:dupliUrl})}
-   
+        let dupliUrl = await urlModel.findOne({ longUrl: longUrl })
+        if (dupliUrl) { return res.status(200).send({ Status: true, msg: dupliUrl }) }
+
         const urlCode = nanoid() //unique
         console.log(urlCode)
 
         shortUrl = 'localhost:3000/' + urlCode.toLowerCase()
-        data = {longUrl:longUrl , shortUrl:shortUrl, urlCode:urlCode}
+        data = { longUrl: longUrl, shortUrl: shortUrl, urlCode: urlCode }
 
         let urlData = await urlModel.create(data)
-        return res.status(201).send({Status:true , msg:urlData})
-
-        
-
-
-
-
+        return res.status(201).send({ Status: true, msg: urlData })
 
     } catch (err) {
         return res.status(500).send({ Status: false, ERROR: err.message })
@@ -50,25 +45,26 @@ const posturl = async function (req, res) {
 
 
 
-const redUrl = async function(req,res){
-    try{
+const redUrl = async function (req, res) {
+    try {
         let urlCode = req.params.urlCode
-        let correctUrlcode = await urlModel.findOne({urlCode:urlCode})
-        if(!correctUrlcode){
-            return res.status(404).send({Status:false, msg:"URL not found. Please enter correct url code"})}
+        let correctUrlcode = await urlModel.findOne({ urlCode: urlCode })
+        if (!correctUrlcode) {
+            return res.status(404).send({ Status: false, msg: "URL not found. Please enter correct url code" })
+        }
+        else {
+            return res.status(302).redirect(correctUrlcode.longUrl)
+        }
 
-            return res.status(302).send({Status :"redirection successful"}).redirect( correctUrlcode.longUrl)
 
 
 
+    } catch (err) {
 
-
-    }catch(err){
-
-        return res.status(500).send({Status:false, ERROR:err.message})
+        return res.status(500).send({ Status: false, ERROR: err.message })
     }
 
 }
 
-module.exports.posturl=posturl
-module.exports.redUrl=redUrl
+module.exports.posturl = posturl
+module.exports.redUrl = redUrl
